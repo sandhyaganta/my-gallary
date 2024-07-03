67770b628f5edef4085212b78a359c3f2dc8a07c
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-07-03 10:23:50 +0530

    employee component completed

--- src/app/emplyees/emplyees.component.ts
+++ src/app/emplyees/emplyees.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-emplyees',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './emplyees.component.html',
   styleUrl: './emplyees.component.css'
 })
 export class EmplyeesComponent {
+  buttonColor: string = 'green';
+  buttonIcon: string = '&#10010;';
+  toggleButton(){
+    if (this.buttonColor === 'green') {
+      this.buttonColor = 'red';
+      this.buttonIcon = '&#9866;'; // minus icon
+    } else {
+      this.buttonColor = 'green';
+      this.buttonIcon = '&#10010;'; // plus icon
+    }
+
+  }
+
+  toggleButton5(){
+    // if (this.buttonColor === 'green') {
+    //   this.buttonColor = 'red';
+    //   this.buttonIcon = '&#9866;'; // minus icon
+    // } else {
+    //   this.buttonColor = 'green';
+    //   this.buttonIcon = '&#10010;'; // plus icon
+    // }
+
+  }
 
 }
